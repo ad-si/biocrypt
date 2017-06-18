2930015228b94f3cd6ef346f0b120a7a4a7259e3
Vincent <jan-vincent.wyszynski@charite.de>
2017-06-18 10:03:07 +0200

    rework buttons

--- biocrypt/src/components/query.vue
+++ biocrypt/src/components/query.vue
@@ -2,41 +2,25 @@
   <div class="query">
     <template v-if="question === 0">
       <p>What is the gender of the patient?</p>
-      <input type="radio" id="male" value="male" v-model="gender">
-      <label for="male">male</label>
-      <br>
-      <input type="radio" id="female" value="female" v-model="gender">
-      <label for="female">female</label>
+      <a v-on:click="next">male</a><a v-on:click="next">female</a>
     </template>
     <template v-if="question === 1">
       <p>Does the patient have dark hair?</p>
-      <input type="radio" id="yes" value="yes" v-model="darkHair">
-      <label for="yes">yes</label>
-      <br>
-      <input type="radio" id="no" value="no" v-model="darkHair">
-      <label for="no">no</label>
+      <a v-on:click="next">yes</a><a v-on:click="next">no</a>
+    </template>
+     <template v-if="question === 2">
+      <p>Is the patient's earlobe attached?</p>
+      <a v-on:click="next">yes</a><a v-on:click="next">no</a>
     </template>
     <template v-if="question === 3">
       <p>Does the patient still have all regular teeth?</p>
-      <input type="radio" id="yes" value="yes" v-model="allTeeth">
-      <label for="yes">yes</label>
-      <br>
-      <input type="radio" id="no" value="no" v-model="allTeeth">
-      <label for="no">no</label>
+      <a v-on:click="next">yes</a><a v-on:click="next">no</a>
     </template>
     <template v-if="question === 4">
-      <p>Can you recognize this feature?</p>
-      <img src="../assets/pic.jpeg">
       <p>Right arm</p>
-      <a v-on:click="next">Yes! Retrieve Patient Infomation</a>
-    </template>
-    <template v-if="question === 2">
-      <p>Is the patient\'s earlobe attached?</p>
-      <input type="radio" id="yes" value="yes" v-model="earlobe">
-      <label for="yes">yes</label>
-      <br>
-      <input type="radio" id="no" value="no" v-model="earlobe">
-      <label for="no">no</label>
+      <img src="../assets/pic.jpeg">
+      <p>Can you recognize this feature?</p>
+      <a v-on:click="next">yes</a><a v-on:click="next">no</a>
     </template>
     <template v-if="question === 5">
       <h2>Markus Petrykowski</h2>
@@ -46,9 +30,6 @@
       <p>Organ Donor: <strong>Yes</strong></p>
       <p>Organs: <strong>All</strong></p>
     </template>
-    <div v-if="question<4">
-      <a v-on:click="next">next</a>
-    </div>
   </div>
 </template>
 
@@ -83,5 +64,12 @@ h1, h2 {
 img {
   max-width: 100%;
   max-height: 100%;
+}
+
+a {
+  border: 1px solid grey;
+  padding: 3px 6px;
+  margin: 10px 15px;
+  display: inline-block;
 }
 </style>
